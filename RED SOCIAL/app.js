910de1019eddbd8e6af6ac1c644fa452d83fb5bf
A.Page <58567281+Tenses@users.noreply.github.com>
2023-03-22 00:09:38 +0100

    Añadir boton de comentar y primer js a la red social

---
+++ RED SOCIAL/app.js
@@ -0,0 +1,34 @@
+/* poner mensaje en post */
+/* el código espera a que se cargue el contenido de la página utilizando window.onload */
+/*     selecciona todos los elementos con la clase "reply" y agrega un controlador de eventos para el evento de clic en cada uno de ellos. Cuando se hace clic en uno de estos botones, se selecciona el elemento hermano siguiente y se cambia su propiedad de visualización a “block” para mostrarlo */
+window.onload = function () {
+    const buttonsReply = document.querySelectorAll(".reply");
+    buttonsReply.forEach((boton) => {
+    boton.addEventListener("click", () => {
+        const reply = boton.nextElementSibling;
+        reply.style.display = "block";
+    });
+    });
+
+/* selecciona todos los elementos con la clase "send-reply" y añade un controlador de eventos para el evento de clic. Al hacer clic en uno de estos botones, se selecciona el valor del elemento hermano anterior textarea, se crea un nuevo elemento p, se establece su contenido como el valor del textarea, y se agrega al final del elemento abuelo (que debería ser el contenedor del mensaje). Luego, borra el valor del textarea y oculta el elemento padre*/
+
+const buttonsSendReply = document.querySelectorAll(".send-reply");
+    buttonsSendReply.forEach((boton) => {
+    boton.addEventListener("click", () => {
+        const reply = boton.previousElementSibling.value;
+        const newParagraph = document.createElement("p");
+        newParagraph.textContent = reply;
+        boton.parentElement.parentElement.appendChild(newParagraph);
+        boton.previousElementSibling.value = "";
+        boton.parentElement.style.display = "none";
+    });
+    });
+
+    /* cierre */
+const botonCerrarRespuesta = document.getElementById("close-button");
+  botonCerrarRespuesta.addEventListener("click", () => {
+    console.log("Botón cerrar clickeado");
+    botonCerrarRespuesta.parentElement.style.display = "none";
+  });
+
+};
