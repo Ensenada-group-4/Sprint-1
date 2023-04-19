c4a56b207ceb17b7de29bdf5086372432e3f6966
Lu <luciasaenz92@hotmail.com>
2023-04-19 15:28:44 +0200

    rename, new folders

---
+++ Keybook/Frontend/javascript/formLogin.js
@@ -0,0 +1,16 @@
+const contactForm = document.getElementById('login-form');
+contactForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const user = event.target.elements['username'].value.trim();
+    const pass = event.target.elements['password'].value.trim();
+    if (user == "Alicia" && pass == "1234")
+    {
+        window.location.href = "./mainPage.html";
+    }
+    else {
+        alert("Usuario y/o contraseña incorrectas");
+    }
+    }
+);
+
+
