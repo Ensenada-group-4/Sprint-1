c8b46df2f71d3cc9e08d9c05a15dacec8fd1df43
Lu <luciasaenz92@hotmail.com>
2023-04-21 11:57:11 +0200

    deleted misleading lines

--- Keybook/Backend/index.js
+++ Keybook/Backend/index.js
@@ -6,17 +6,5 @@ const port = 3000;
 
 
 app.use(express.json())
-app.use(express.static(path.join("..", "Frontend")));
-// app.use(express.static("Keybook"));
-// app.use(express.static("public"));
-
-
-
-app.get("/", (req, res) => {  
-    res.sendFile(path.join(__dirname, "..", "Frontend", "html", "formLogin.html"))
-})
-
-
-
 
 app.listen(port, () => { console.log(`Server listening on port ${port}`) });
