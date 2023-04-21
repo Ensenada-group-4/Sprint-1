5356334e57c947e543d4bbe014f2e31a97a081fc
Lu <luciasaenz92@hotmail.com>
2023-04-21 10:59:50 +0200

    html ok css js pending

--- Keybook/Backend/index.js
+++ Keybook/Backend/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const path = require("path")
+
 const app = express();
 const port = 3000;
 
+
 app.use(express.json())
-// app.use(express.static(path.join(__dirname, 'Frontend')));
-// app.use(express.static(__dirname + "/Frontend"));
+app.use(express.static(path.join("..", "Frontend")));
+// app.use(express.static("Keybook"));
 // app.use(express.static("public"));
+
+
+
+app.get("/", (req, res) => {  
+    res.sendFile(path.join(__dirname, "..", "Frontend", "html", "formLogin.html"))
+})
+
 
 
 
