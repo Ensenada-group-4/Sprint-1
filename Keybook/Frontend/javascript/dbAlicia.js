e62cd9d85a097bddb64e8d8de532355772904d10
A.Page <58567281+Tenses@users.noreply.github.com>
2023-04-27 20:31:09 +0200

    alicia try cacth

--- Keybook/Frontend/javascript/dbAlicia.js
+++ Keybook/Frontend/javascript/dbAlicia.js
@@ -1,32 +1,38 @@
 
-async function getUsers(done) {
-    fetch('http://localhost:3000/user/id_5')
+async function getUser(done) {
+    const response = await fetch("http://localhost:3000/user/id_5");
+    const data = await response.json()
 
-        .then(response => response.json())
-        .then(data => {
-            console.log(data[0].name)
-            console.log(data.name)
+    try {
+        console.log(data[0].name)
+        console.log(data.name)
 
-            const nameField = document.getElementById('profile-fullName');
-            nameField.innerHTML = data[0].name + ' ' + data[0].last_name;
+        const nameField = document.getElementById('profile-fullName');
+        nameField.innerHTML = data[0].name + ' ' + data[0].last_name;
 
-            const birthField = document.getElementById('profile-birthDate');
-            birthField.innerHTML = data[0].date_of_birth
+        const birthField = document.getElementById('profile-birthDate');
+        birthField.innerHTML = data[0].date_of_birth
 
-            const cityField = document.getElementById('profile-location');
-            cityField.textContent = data[0].city
+        const cityField = document.getElementById('profile-location');
+        cityField.textContent = data[0].city
 
-            const countryField = document.getElementById('profile-loc-country');
-            countryField.textContent = data[0].country
+        const countryField = document.getElementById('profile-loc-country');
+        countryField.textContent = data[0].country
 
-            const emailField = document.getElementById('profile-email');
-            emailField.textContent = data[0].email
-            console.log(response);
-            done();
-        })
-        .catch((err) => console.log(err));
+        const emailField = document.getElementById('profile-email');
+        emailField.textContent = data[0].email
+
+        const phoneField = document.getElementById('profile-phone');
+        phoneField.textContent = data[0].phone
+
+        console.log(response);
+        done();
+    }
+    catch (error) {
+        console.log('Ocurrio un error al solicitar los datos', error)
+    }
 }
 
-getUsers(() => {
+getUser(() => {
     console.log('Datos de usuario cargados');
 });
