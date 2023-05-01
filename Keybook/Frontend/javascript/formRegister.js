41d2c2189a1369c658368d693f7d53ee846ecf49
A.Page <58567281+Tenses@users.noreply.github.com>
2023-05-01 19:42:25 +0200

    formulario registro dle front

--- Keybook/Frontend/javascript/formRegister.js
+++ Keybook/Frontend/javascript/formRegister.js
@@ -1,9 +1,23 @@
 //en proyecto, no es funcional
-const form = document.querySelector('form');
-const formData = new FormData();
+const form = document.querySelector("form-register");
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-async function formRegister() {
-    fetch('')
+    const name = document.querySelector("#name").value;
+    const password = document.querySelector("#password").value;
+
+    const data = {
+        name,
+        password
+    };
+
+    fetch("http://localhost:3000/user", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
         .then(response => response.json())
         .then(data => {
             console.log(data);
@@ -11,4 +25,4 @@ async function formRegister() {
         .catch(error => {
             console.error(error);
         });
-}
+});
