cba85af14ad44280de1f9cd7335aad47a17bb6a0
Lu <luciasaenz92@hotmail.com>
2023-05-05 19:59:50 +0200

    cambios index

--- Keybook/Backend/index.js
+++ Keybook/Backend/index.js
@@ -100,9 +100,9 @@ app.post("/register", async function (req, res) {
 
 //POST login 
 app.post("/auth", async (req, res) => {
-    const { user, password } = req.body;
+    const { user } = req.body;
     const result = await sequelize.query(
-        `SELECT * FROM user WHERE (name = '${user.name}' OR email = '${user.email}') AND password = '${password}'`
+        `SELECT * FROM user WHERE (name = '${user.name}' OR email = '${user.email}') AND password = '${user.password}'`
     );
     if (result[0].length) {
         res.status(200).send({ id: result[0][0].id });
