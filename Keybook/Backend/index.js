86c38e51a03a729e089d6b220c818ea0b3632c7d
A.Page <58567281+Tenses@users.noreply.github.com>
2023-05-05 11:01:46 +0200

    index userID y post ID intento

--- Keybook/Backend/index.js
+++ Keybook/Backend/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
 const sequelize = require('./db/connection.js');
 const bodyParser = require("body-parser")
 var cors = require('cors')
@@ -10,12 +10,12 @@ app.use(cors());
 app.use(bodyParser.json())
 
 // GET home page
-app.get('/', function(req, res, next) {
+app.get('/', function (req, res, next) {
     res.send("Keybook server");
-  });
+});
 
 //GET users list
-app.get('/users', async function (req, res) {   
+app.get('/users', async function (req, res) {
     try {
         const usersList = await sequelize.query("SELECT * FROM user", { type: sequelize.QueryTypes.SELECT });
         console.log(usersList);
@@ -76,7 +76,7 @@ app.post("/register", async function (req, res) {
             {
                 type: sequelize.QueryTypes.INSERT,
                 replacements: [
-                    name,                    
+                    name,
                     email,
                     password
                 ],
@@ -85,7 +85,7 @@ app.post("/register", async function (req, res) {
         res.status(200)
             .send({
                 user_id: newUser[0],
-                name,                
+                name,
                 email,
                 password
             })
@@ -101,37 +101,33 @@ app.post("/register", async function (req, res) {
 app.post("/auth", async (req, res) => {
     const { user, password } = req.body;
     const result = await sequelize.query(
-      `SELECT * FROM user WHERE (name = '${user.name}' OR email = '${user.email}') AND password = '${password}'`
+        `SELECT * FROM user WHERE (name = '${user.name}' OR email = '${user.email}') AND password = '${password}'`
     );
     if (result[0].length) {
-      res.status(200).send({ id: result[0][0].id });
+        res.status(200).send({ id: result[0][0].id });
     } else {
-      res.status(400).send({ error: "Usuario o password incorrecto" });
+        res.status(400).send({ error: "Usuario o password incorrecto" });
     }
-  });
-  
+});
+
 
 //POST posts
 app.post("/posts", async function (req, res) {
     console.log("req.body", req.body);
     try {
         const { post_content } = req.body;
-        const post_id_user = 1;
-        // const post_date = new Date().toString();
+        const userId = req.session.userId; // retrieve user ID from session
         const newPost = await sequelize.query(
             `INSERT INTO post (post_id_user, post_content) VALUES (?, ?)`,
             {
                 type: sequelize.QueryTypes.INSERT,
-                replacements: [
-                    post_id_user,
-                    post_content
-                ],
+                replacements: [userId, post_content],
             }
         );
         res.status(200).send({
             post_id: newPost[0],
-            post_id_user,
-            post_content
+            post_id_user: userId,
+            post_content,
         });
     } catch (e) {
         console.log(e);
@@ -155,14 +151,27 @@ app.get('/posts', async function (req, res) {
     }
 });
 
+//endpoint para los user.id del login
+app.get("/users/:userId", async (req, res) => {
+    const userId = req.params.userId;
+    const result = await sequelize.query(
+        `SELECT * FROM user WHERE id = ${userId}`
+    );
+    if (result[0].length) {
+        res.status(200).send(result[0][0]);
+    } else {
+        res.status(404).send({ error: "Usuario no encontrado" });
+    }
+});
+
 //TODO GET users by input
 // app.get("/user", async function (req, res) {
 //     const { searchKey } = req.query;
 //     console.log("instance");
 //     try {
-  
+
 //       cond?true:false
-  
+
 //       const personas = searchKey
 //         ? await sequelize.query(
 //             `SELECT * FROM user WHERE name % ${searchKey} OR email % ${searchKey}  `,
