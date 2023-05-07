59107b4dfee75db04a3b97a67a088ab316ac709b
A.Page <58567281+Tenses@users.noreply.github.com>
2023-05-07 20:05:16 +0200

    put funcional email y phone

--- Keybook/Frontend/javascript/editProfile.js
+++ Keybook/Frontend/javascript/editProfile.js
@@ -1,31 +1,67 @@
-const form = document.querySelector("#email-edit");
-
+const form = document.querySelector("#email-edit-form");
 
 form.addEventListener("submit", async (event) => {
     event.preventDefault();
 
     const id = localStorage.getItem('userId');
-    const newEmail = document.querySelector("#new-email").value;
+    const email = document.getElementById("new-email").value;
 
     const data = {
-        id,
-        newEmail,
+        email
     };
+    console.log(data)
+    console.log(email)
 
-    await fetch("http://localhost:3000/users/email", {
+
+    await fetch(`http://localhost:3000/users/${id}/email`, {
         method: "PUT",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify(data)
     })
-        .then(response => response.json()
-        )
+        .then(response => response.json())
         .then(data => {
             console.log(data);
+            console.log(id)
+
             alert('email actualizado correctamente');
-            const emailField = document.getElementById('profile-email');
-            emailField.innerHTML = newEmail;
+            // const emailField = document.getElementById('profile-email');
+            // emailField.innerHTML = newEmail;
+            location.reload();
+        })
+        .catch(error => {
+            console.error(error);
+        });
+});
+
+const form_phone = document.querySelector("#phone-edit-form");
+
+form_phone.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const id = localStorage.getItem('userId');
+    const phone = document.getElementById("new-phone").value;
+
+    const data = {
+        phone
+    };
+    console.log(data)
+    console.log(phone)
+
+    await fetch(`http://localhost:3000/users/${id}/phone`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+        .then(response => response.json())
+        .then(data => {
+
+            alert('Teféfono actualizado correctamente');
+            // const emailField = document.getElementById('profile-email');
+            // emailField.innerHTML = newEmail;
             location.reload();
         })
         .catch(error => {
