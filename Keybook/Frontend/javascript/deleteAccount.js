837595bef368d41170eb3923b17d493eae1ff14f
Lu <luciasaenz92@hotmail.com>
2023-05-08 10:43:32 +0200

    delete funcional OK

--- Keybook/Frontend/javascript/deleteAccount.js
+++ Keybook/Frontend/javascript/deleteAccount.js
@@ -1,24 +1,31 @@
-//borrar cuenta
+//BORRAR CUENTA
 const eliminarCuenta = document.querySelector('#eliminar-cuenta');
 const container = document.querySelector("#cuenta");
 const cards = container.querySelectorAll(".profile-data");
 
-eliminarCuenta.addEventListener('click', () => {
+eliminarCuenta.addEventListener('click', async (event) => {
+    event.preventDefault();
+
+    const id = localStorage.getItem('userId');
+
     if (confirm('¿Estás seguro de que deseas eliminar tu cuenta?')) {
-        cards.forEach(card => {
-            card.innerHTML = "";            
-            setTimeout(() => {
-                window.location.href = "formLogin.html"
-            }, 5000)
-        })
+        const response = await fetch(`http://localhost:3000/users/${id}/delete`, {
+            method: "DELETE",
+            headers: {
+                "content-type": "application/json",
+            },            
+        }
+        )
+            .then(() => {
+                localStorage.removeItem('userId')
+                cards.forEach(card => {
+                    card.innerHTML = ""
+                })
+                alert("Cuenta borrada. Te echaremos de menos 😔")
+                setTimeout(() => {
+                    window.location.href = "formLogin.html"
+                }, 3000)
+            })
+            .catch(console.error())
     }
 })
-
-
-//PARA USAR EN UN FUTURO CON APIS
-
-// fetch('/eliminar-cuenta', {
-// method: 'DELETE'
-// })
-// .then(() => {
-// window.location.href = 'formLogin.html'
